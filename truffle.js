1936657d907ca6e2f31c04630944109a3e34845f
Lucas Bertrand <lucas.bertrand@ensimag.grenoble-inp.fr>
2018-04-28 18:18:42 +0200

    Increasing truffle gas

--- truffle.js
+++ truffle.js
@@ -4,7 +4,7 @@ module.exports = {
       host: "localhost",
       port: 7545,
       network_id: "*",
-      // gas: 4712388,
+      gas: 4712388,
       gasPrice: 2000000000,
       from: '0x627306090abaB3A6e1400e9345bC60c78a8BEf57'
     }
