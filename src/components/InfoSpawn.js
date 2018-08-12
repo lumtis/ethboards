6fe115a1d29139ae7ecaf3b9269b72c668aedb96
Lucas Bertrand <lucas.bertrand@ensimag.grenoble-inp.fr>
2018-08-12 16:57:06 +0200

    Info spawner

---
+++ src/components/InfoSpawn.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+
+
+const infoStyle = {
+  position: 'absolute',
+  padding: '20px',
+  width: '400px',
+  minHeight: '100px',
+  backgroundColor: 'rgba(240, 240, 240, 0.7)',
+  marginRight: 'auto',
+  marginLeft: 'auto',
+  marginBottom: '20px',
+  zIndex: 1000
+};
+
+
+class InfoSpawn extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleMouseHover = this.handleMouseHover.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+
+    this.state = {
+      isHovering: false
+    }
+  }
+
+  static defaultProps = {
+    infoContent: ''
+  }
+
+  componentWillMount() {
+  }
+
+  handleMouseHover() {
+    this.setState({isHovering: true});
+  }
+
+  handleMouseLeave() {
+    this.setState({isHovering: false});
+  }
+
+  render() {
+    if (this.state.isHovering) {
+      return (
+        <div onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseLeave}>
+          <img src="/images/info.png" alt="Nuja" style={{height: '15px'}}></img>
+          <div style={infoStyle}>
+            <div className="row" style={{padding: '10px'}}>
+              <p>{this.props.infoContent}</p>
+            </div>
+          </div>
+        </div>
+      )
+    }
+    else {
+      return (
+        <div onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseLeave}>
+          <img src="/images/info.png" alt="Nuja" style={{height: '15px'}}></img>
+        </div>
+      )
+    }
+  }
+}
+
+export default InfoSpawn
