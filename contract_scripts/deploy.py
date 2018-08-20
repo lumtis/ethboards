48c6a86e02b8129435f9f2b0e88dae1c728f2450
Lucas Bertrand <lucas.bertrand@ensimag.grenoble-inp.fr>
2018-08-20 15:18:38 +0200

    New deploy script for test

--- contract_scripts/deploy.py
+++ contract_scripts/deploy.py
@@ -8,45 +8,6 @@ from time import sleep
 web3 = Web3(HTTPProvider('http://localhost:8545'))
 
 
-
-# Deploying TimeoutStarter
-jsonData=open('../build/contracts/ServerManager.json').read()
-data = json.loads(jsonData)
-contract = web3.eth.contract(abi=data['abi'], bytecode=data['bytecode'])
-tx_hash = contract.deploy(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000, 'gas': 6721975000})
-sleep(1)
-tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
-addr = tx_receipt['contractAddress']
-print('ServerManager')
-ServerManagerAddress = addr
-print(addr)
-
-
-# Deploying TimeoutStarter
-jsonData=open('../build/contracts/TimeoutStarter.json').read()
-data = json.loads(jsonData)
-contract = web3.eth.contract(abi=data['abi'], bytecode=data['bytecode'])
-tx_hash = contract.deploy(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000, 'gas': 6721975000})
-sleep(1)
-tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
-addr = tx_receipt['contractAddress']
-print('TimeoutStarter')
-TimeoutStarterAddress = addr
-print(addr)
-
-# Deploying TimeoutManager
-jsonData=open('../build/contracts/TimeoutStopper.json').read()
-data = json.loads(jsonData)
-contract = web3.eth.contract(abi=data['abi'], bytecode=data['bytecode'])
-tx_hash = contract.deploy(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000, 'gas': 6721975000})
-sleep(1)
-tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
-addr = tx_receipt['contractAddress']
-print('TimeoutStopper')
-TimeoutStopperAddress = addr
-print(addr)
-
-
 # Deploying NujaRegistry
 jsonData=open('../build/contracts/NujaRegistry.json').read()
 data = json.loads(jsonData)
@@ -84,6 +45,43 @@ WeaponRegistryAddress = addr
 print(addr)
 
 
+# Deploying ServerManager
+jsonData=open('../build/contracts/ServerManager.json').read()
+data = json.loads(jsonData)
+contract = web3.eth.contract(abi=data['abi'], bytecode=data['bytecode'])
+tx_hash = contract.deploy(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000, 'gas': 6721975000})
+sleep(1)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+addr = tx_receipt['contractAddress']
+print('ServerManager')
+ServerManagerAddress = addr
+print(addr)
+
+# Deploying TimeoutStarter
+jsonData=open('../build/contracts/TimeoutStarter.json').read()
+data = json.loads(jsonData)
+contract = web3.eth.contract(abi=data['abi'], bytecode=data['bytecode'])
+tx_hash = contract.deploy(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000, 'gas': 6721975000})
+sleep(1)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+addr = tx_receipt['contractAddress']
+print('TimeoutStarter')
+TimeoutStarterAddress = addr
+print(addr)
+
+# Deploying TimeoutStopper
+jsonData=open('../build/contracts/TimeoutStopper.json').read()
+data = json.loads(jsonData)
+contract = web3.eth.contract(abi=data['abi'], bytecode=data['bytecode'])
+tx_hash = contract.deploy(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000, 'gas': 6721975000})
+sleep(1)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+addr = tx_receipt['contractAddress']
+print('TimeoutStopper')
+TimeoutStopperAddress = addr
+print(addr)
+
+
 # Deploying NujaBattle
 jsonData=open('../build/contracts/NujaBattle.json').read()
 data = json.loads(jsonData)
@@ -97,12 +95,13 @@ NujaBattleAddress = addr
 print(addr)
 
 
-# Setting registry for characterRegistry
-jsonData=open('../build/contracts/CharacterRegistry.json').read()
+# Setting addresses for serverManager
+jsonData=open('../build/contracts/serverManager.json').read()
 data = json.loads(jsonData)
-cr = web3.eth.contract(data['abi'], CharacterRegistryAddress)
-tx_hash = cr.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).changeNujaRegistry(NujaRegistryAddress)
+tm = web3.eth.contract(data['abi'], ServerManagerAddress)
+tx_hash = tm.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).setAddresses(NujaBattleAddress)
 tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+
 
 # Setting addresses for timeoutStarter
 jsonData=open('../build/contracts/TimeoutStarter.json').read()
@@ -111,13 +110,12 @@ tm = web3.eth.contract(data['abi'], TimeoutStarterAddress)
 tx_hash = tm.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).setAddresses(NujaBattleAddress, TimeoutStopperAddress)
 tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
 
-# Setting addresses for timeoutManager
+# Setting addresses for timeoutStopper
 jsonData=open('../build/contracts/TimeoutStopper.json').read()
 data = json.loads(jsonData)
 tm = web3.eth.contract(data['abi'], TimeoutStopperAddress)
 tx_hash = tm.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).setAddresses(NujaBattleAddress, TimeoutStarterAddress)
 tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
-
 
 
 # Deploying Nujas
@@ -258,6 +256,36 @@ addr = tx_receipt['contractAddress']
 print('Sword')
 SwordAddress = addr
 print(addr)
+jsonData=open('../build/contracts/Missile.json').read()
+data = json.loads(jsonData)
+contract = web3.eth.contract(abi=data['abi'], bytecode=data['bytecode'])
+tx_hash = contract.deploy(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000})
+sleep(1)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+addr = tx_receipt['contractAddress']
+print('Sword')
+MissileAddress = addr
+print(addr)
+jsonData=open('../build/contracts/Motorcycle.json').read()
+data = json.loads(jsonData)
+contract = web3.eth.contract(abi=data['abi'], bytecode=data['bytecode'])
+tx_hash = contract.deploy(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000})
+sleep(1)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+addr = tx_receipt['contractAddress']
+print('Sword')
+MotorcycleAddress = addr
+print(addr)
+jsonData=open('../build/contracts/Healthpack.json').read()
+data = json.loads(jsonData)
+contract = web3.eth.contract(abi=data['abi'], bytecode=data['bytecode'])
+tx_hash = contract.deploy(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000})
+sleep(1)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+addr = tx_receipt['contractAddress']
+print('Sword')
+HealthpackAddress = addr
+print(addr)
 
 # Registering Weapons
 jsonData=open('../build/contracts/WeaponRegistry.json').read()
@@ -277,38 +305,68 @@ tx_hash = wr.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116D
 tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
 tx_hash = wr.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).addWeapon(SwordAddress)
 tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
-
-
+tx_hash = wr.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).addWeapon(MissileAddress)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+tx_hash = wr.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).addWeapon(MotorcycleAddress)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+tx_hash = wr.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).addWeapon(HealthpackAddress)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
 
 
 # Creating character
 jsonData=open('../build/contracts/CharacterRegistry.json').read()
 data = json.loads(jsonData)
 cr = web3.eth.contract(data['abi'], CharacterRegistryAddress)
-tx_hash = cr.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).addCharacter('Haiti', '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 0)
+tx_hash = cr.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).addCharacter('Haiti', '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 1)
 tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
 tx_hash = cr.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).addCharacter('France', '0xaBD29D4A1FCCBC958Bff35F5D76661Ce6edbC69F', 4)
 tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
-tx_hash = cr.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).addCharacter('France', '0xcb28d25F51D1649be26f6c98f6d8f8395ff0C6DB', 3)
+tx_hash = cr.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).addCharacter('Babu', '0xcb28d25F51D1649be26f6c98f6d8f8395ff0C6DB', 3)
 tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+tx_hash = cr.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).addCharacter('Kaito', '0xeAf8aD9e18289b45adDA6638Baf41Bd92a290790', 0)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+tx_hash = cr.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).addCharacter('Krim', '0xF5a4d99C001C816fC1DE374D5177e5aC0d6bA965', 1)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+tx_hash = cr.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).addCharacter('TUghuL', '0x17Bfe729Ae7ec0aCb977085E7892fbafb13Da0B6', 2)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+tx_hash = cr.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).addCharacter('3489034', '0x9A36D4859D225C476bA7489E15Aa06aA9899F343', 4)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+tx_hash = cr.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).addCharacter('BTUG', '0x79C7Dd643199f5ef4B7C93b464d263A5860aA39f', 0)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+
 
 jsonData=open('../build/contracts/ServerManager.json').read()
 data = json.loads(jsonData)
 sm = web3.eth.contract(data['abi'], ServerManagerAddress)
 
-nineArray = [0,0,0,0,0,0,0,0,0]
+sixArray = [0,0,0,0,0,0]
+sixArrayString = [ '', '', '', '', '', '']
 
-tx_hash = sm.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000, 'value': 5000000000000000}).addServer('toast', 3, 0, 0)
+tx_hash = sm.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000, 'value': 5000000000000000}).addServer('toast', 8, 0, 0)
 tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
-tx_hash = sm.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).addBuildingToServer(0, [3]+nineArray, [3]+nineArray, [2]+nineArray, 1)
+
+
+tx_hash = sm.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).addBuildingToServer(0, [0,0,1,1]+sixArray, [0,1,0,1]+sixArray, [6,7,8,9]+sixArray, [ 'Utapia',  'Fraland',  'Ramloc',  'Magmapop']+sixArrayString, 4)
 tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
-tx_hash = sm.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).setServerOnline(0)
+tx_hash = sm.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000}).changeServerState(0)
 tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+
 tx_hash = sm.transact(transaction={'from': '0xDf907B2794f0b6B2967d4035464edA116DDc2578', 'gasPrice': 2000000000, 'value': 5000000000000000}).addPlayerToServer(0, 0)
 tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
 tx_hash = sm.transact(transaction={'from': '0xaBD29D4A1FCCBC958Bff35F5D76661Ce6edbC69F', 'gasPrice': 2000000000, 'value': 5000000000000000}).addPlayerToServer(1, 0)
 tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
 tx_hash = sm.transact(transaction={'from': '0xcb28d25F51D1649be26f6c98f6d8f8395ff0C6DB', 'gasPrice': 2000000000, 'value': 5000000000000000}).addPlayerToServer(2, 0)
 tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+tx_hash = sm.transact(transaction={'from': '0xeAf8aD9e18289b45adDA6638Baf41Bd92a290790', 'gasPrice': 2000000000, 'value': 5000000000000000}).addPlayerToServer(3, 0)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+tx_hash = sm.transact(transaction={'from': '0xF5a4d99C001C816fC1DE374D5177e5aC0d6bA965', 'gasPrice': 2000000000, 'value': 5000000000000000}).addPlayerToServer(4, 0)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+tx_hash = sm.transact(transaction={'from': '0x17Bfe729Ae7ec0aCb977085E7892fbafb13Da0B6', 'gasPrice': 2000000000, 'value': 5000000000000000}).addPlayerToServer(5, 0)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+tx_hash = sm.transact(transaction={'from': '0x9A36D4859D225C476bA7489E15Aa06aA9899F343', 'gasPrice': 2000000000, 'value': 5000000000000000}).addPlayerToServer(6, 0)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+tx_hash = sm.transact(transaction={'from': '0x79C7Dd643199f5ef4B7C93b464d263A5860aA39f', 'gasPrice': 2000000000, 'value': 5000000000000000}).addPlayerToServer(7, 0)
+tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+
 tx_hash = sm.transact(transaction={'from': '0xcb28d25F51D1649be26f6c98f6d8f8395ff0C6DB', 'gasPrice': 2000000000}).startServer(0)
 tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
