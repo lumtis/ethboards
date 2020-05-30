4bd76863d5500cbbc3ce5579c8dc2ffc195ce390
ltacker <lucas.bertrand.22@gmail.com>
2020-05-30 20:30:00 +0800

    Replace server by boards

--- contracts/BoardList.sol
+++ contracts/BoardList.sol
@@ -8,15 +8,13 @@ import "NujaRegistry.sol";
 import "Nuja.sol";
 import "WeaponRegistry.sol";
 import "Weapon.sol";
-import "Geometry.sol";
 import "StateManager.sol";
 import "NujaBattle.sol";
 
 
-contract ServerManager is Geometry, StateManager {
+contract BoardList {
 
     // General values
-    address owner;
     address characterRegistry;
     address weaponRegistry;
     address nujaBattle;
@@ -27,11 +25,6 @@ contract ServerManager is Geometry, StateManager {
     ///////////////////////////////////////////////////////////////
     /// Modifiers
 
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-
     modifier fromNujaBattle {
         require(msg.sender == nujaBattle);
         _;
@@ -41,35 +34,26 @@ contract ServerManager is Geometry, StateManager {
     ///////////////////////////////////////////////////////////////
     /// Structures
 
-    struct Player {
-        address owner;
-        uint characterIndex;
-        uint8 initialX;
-        uint8 initialY;
+    struct PawnPosition {
+        uint8 pawnType;
+        uint8 x;
+        uint8 y;
     }
 
-    struct Building {
-        uint8 weapon;
-        bytes32 name;
-    }
-
-    struct Server {
+    struct Board {
         uint id;
-        string name;
         address owner;
-        uint fee;
-        uint moneyBag;
-        uint currentMatchId; // Warning: offset
-        uint8 playerMax;
-        uint8 playerNb;
-        uint8 state;       // 0: offline, 1: waiting, 2: running
-        mapping (uint8 => Building) buildings;
-        mapping (uint8 => Player) players;
-        mapping (address => uint8) playerIndex;   // Warning: offset
+        string name;
+        uint gameCount;
+        bool deployed;
+        uint8 pawnTypeNumber;
+        uint8 pawnNumber;
+        mapping (uint8 => address) pawnTypeAddress;
+        mapping (uint8 => PawnPosition) pawnPosition;
     }
 
-    uint serverNumber;
-    Server[] servers;
+    uint boardNumber;
+    Board[] boards;
 
     // Get the server associated with the match id
     // The value is server.id + 1 because 0 represents not started yet match or ended match
@@ -77,7 +61,6 @@ contract ServerManager is Geometry, StateManager {
     uint matchNb;
 
     // Necessary to get user's server
-    mapping (address => uint) serverUserNumber;
     mapping (address => mapping (uint => uint)) serverUserIndex;
 
     // Map character to server
@@ -85,14 +68,9 @@ contract ServerManager is Geometry, StateManager {
 
     ///////////////////////////////////////////////////////////////
 
-    function ServerManager() public {
-        owner = msg.sender;
-        serverNumber = 0;
-        characterRegistry = 0x462893f08BbaED3319a44E613E57e5257b0E5037;
-        weaponRegistry = 0xDF480F0D91C0867A0de18DA793486287A22c2243;
+    function BoardList() public {
+        boardNumber = 0;
         nujaBattle = address(0);
-        serverCreationFee = 5 finney;
-        cheatWarrant = 5 finney;
         matchNb= 0;
         addressesSet = false;
     }
@@ -104,11 +82,6 @@ contract ServerManager is Geometry, StateManager {
         require(addressesSet == false);
         nujaBattle = nujaBattle_;
         addressesSet = true;
-    }
-
-    function changeFeeAndCheatWarrant(uint fee, uint warrant) public onlyOwner {
-        serverCreationFee = fee * 1 finney;
-        cheatWarrant = warrant * 1 finney;
     }
 
     // Add server to the server list
@@ -135,25 +108,6 @@ contract ServerManager is Geometry, StateManager {
 
         // Transfer fee to contract owner
         owner.transfer(msg.value);
-    }
-
-    // Set the server online if server offline and offline if server online
-    function changeServerState(uint indexServer) public {
-        require(indexServer < serverNumber);
-        require(servers[indexServer].owner == msg.sender);
-
-        if(servers[indexServer].state == 0) {
-            servers[indexServer].state = 1;
-        }
-        else if(servers[indexServer].state == 1) {
-            // If player has already joined the server, it can't be offline
-            require(servers[indexServer].playerNb == 0);
-            servers[indexServer].state = 0;
-        }
-        else {
-            // A match is running on the server, the state cannot be changed
-            revert();
-        }
     }
 
     // Add list of buildings to the server
@@ -403,118 +357,23 @@ contract ServerManager is Geometry, StateManager {
     }
 
 
-    // Specific server information
 
-    // Get the initial state of server (considering building, players position etc)
-    function getInitialState(uint indexServer) public view returns(uint8[176] ret) {
-        require(indexServer < serverNumber);
+    // Get the initial state of the board
+    function getInitialState(uint boardId) public view returns(uint8[121] state) {
+        require(boardId < boardNumber, "The board doesn't exist");
 
-        // Buildings
-        for(uint8 i = 0; i<8; i++) {
-            for(uint8 j = 0; j<8; j++) {
-                ret[i*8+j] = servers[indexServer].buildings[i*8+j].weapon;
-            }
-        }
-        // Players
-        for(i = 0; i<8; i++) {
-            for(j = 0; j<8; j++) {
-                ret[64+i*8+j] = 0;
-            }
-        }
-        // healths
-        for(i = 0; i<servers[indexServer].playerMax; i++) {
-            ret[128+i] = 100;
-        }
-        for(i = servers[indexServer].playerMax; i<8; i++) {
-            ret[128+i] = 0;
-        }
-        // Positions
-        for(i = 0; i<servers[indexServer].playerMax; i++) {
-            ret[136+i] = servers[indexServer].players[i].initialX;
-            ret[144+i] = servers[indexServer].players[i].initialY;
-            ret[64+servers[indexServer].players[i].initialX*8+servers[indexServer].players[i].initialY] = i+1;
-        }
-        for(i = servers[indexServer].playerMax; i<8; i++) {
-            ret[136+i] = 0;
-            ret[144+i] = 0;
-        }
-        // Weapons
-        for(i = 0; i<24; i++) {
-            ret[152+i] = 0;
+        // Pawn number
+        state[0] = boards[boardId].pawnNumber;
+
+        for(i = 0; i<boards[boardId].pawnNumber; i++) {
+             // Pawn type
+            state[1+i] = boards[boardId].PawnPosition[i].pawnType;
+            // Pawn x position
+            state[41+i] = boards[boardId].PawnPosition[i].x;
+            // Pawn y position
+            state[81+i] = boards[boardId].PawnPosition[i].y;
         }
 
-        return ret;
-    }
-
-    // Get user index in server from his address
-    function getIndexFromAddress(uint indexServer, address ownerAddress) public view returns(uint8 indexRet) {
-        require(indexServer < serverNumber);
-        require(servers[indexServer].playerIndex[ownerAddress] > 0);
-
-        return servers[indexServer].playerIndex[ownerAddress]-1;
-    }
-
-    // Get address in a server from the index
-    function getAddressFromIndex(uint indexServer, uint8 indexPlayer) public view returns(address ownerRet) {
-        require(indexServer < serverNumber);
-        require(indexPlayer < servers[indexServer].playerMax);
-
-        return servers[indexServer].players[indexPlayer].owner;
-    }
-
-    // Check if user is present in the server
-    function isAddressInServer(uint indexServer, address ownerAddress) public view returns(bool isRet) {
-        require(indexServer < serverNumber);
-
-        return (servers[indexServer].playerIndex[ownerAddress] > 0);
-    }
-
-    // Get Character index from server and player index
-    function playerCharacter(uint indexServer, uint8 indexPlayer) public view returns(uint characterIndex) {
-        require(indexServer < serverNumber);
-        require(indexPlayer < servers[indexServer].playerMax);
-
-        return (servers[indexServer].players[indexPlayer].characterIndex);
-    }
-
-    // Get the current server from character
-    function getCharacterServer(uint characterId) public view returns(uint serverId) {
-        // Verify character exists
-        CharacterRegistry reg = CharacterRegistry(characterRegistry);
-        require(characterId < reg.totalSupply());
-
-        return characterServer[characterId];
-    }
-
-
-    // Server modification
-
-    // Remove player from server
-    function removePlayer(uint indexServer, uint8 killed) public fromNujaBattle {
-        servers[indexServer].playerNb -= 1;
-
-        // Set player index to 0
-        servers[indexServer].playerIndex[servers[indexServer].players[killed].owner] = 0;
-
-        // Set character server to 0
-        uint character = servers[indexServer].players[killed].characterIndex;
-        characterServer[character] = 0;
-    }
-
-    // Terminate the running server
-    function terminateServer(uint indexServer, uint8 winner) public fromNujaBattle {
-        // Reset server
-        removePlayer(indexServer, winner);
-        servers[indexServer].state = 1;
-        serverMatch[servers[indexServer].currentMatchId-1] = 0;
-        servers[indexServer].currentMatchId = 0;
-
-        // Winner get his money back
-        servers[indexServer].players[winner].owner.transfer(servers[indexServer].moneyBag + cheatWarrant);
-    }
-
-    // Transfer of fund approved by nuja battle smart contract
-    function nujaBattleTransfer(address addr, uint amount) public fromNujaBattle {
-        addr.transfer(amount);
+        return state;
     }
 }
