7e0bfff6dc4eee943ee985fdc852921fd4ebd216
ltacker <lucas.bertrand.22@gmail.com>
2020-06-01 12:14:08 +0800

    Update truffle version

--- truffle.js
+++ truffle.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 7545,
+      port: 8545,
       network_id: "*",
       gas: 4712388,
       gasPrice: 2000000000,
