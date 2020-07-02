92ca5643d087e2e6ace56e8f4cffefcc7c42ad84
ltacker <lucas.bertrand.22@gmail.com>
2020-07-02 17:04:02 +0800

    Solved bug in web3 call functin

--- src/utils/gameUtils.js
+++ src/utils/gameUtils.js
@@ -1,14 +1,23 @@
+import {getPawnPosition} from './stateUtils'
+
 // Test if a simulation work
 // Return false if the smart contract method reverts
 export const testSimulate = async (drizzle, boardId, player, move, state) => {
     try {
-        await drizzle.contracts.EthBoards.methods.simulate(
+        const outState = await drizzle.contracts.EthBoards.methods.simulate(
             drizzle.contracts.BoardHandler.options.address,
             boardId,
             player,
             move,
             state
         ).call()
+
+        // when using some networks like Rinkeby, no error is thrown, an incoherent value is returned
+        // Check here if the incoherent value is returned
+        // TODO: Correctly handle this error
+        if (parseInt(outState[0]) > 255) {
+            return null
+        }
 
         return [move[2], move[3]]
     } catch (err) {
