5822dd275333c83818c36f6608947c431793bf07
ltacker <lucas.bertrand.22@gmail.com>
2020-07-08 15:17:45 +0800

    Function for timeout

--- contracts/EthBoards.sol
+++ contracts/EthBoards.sol
@@ -8,6 +8,27 @@ import "./StateController.sol";
 contract EthBoards {
     using StateController for uint8[121];
 
+    uint constant timeoutTime = 5 minutes;
+
+    struct Turn {
+        uint8[4] move;
+        bytes32 r,
+        bytes32 s,
+        uint8 v
+    }
+
+    // Store the current turn with a started timeout for the specific game
+    mapping (uint => mapping (uint => uint)) timeoutTurnNumber;
+
+    // Store the timestamp of the timeout for the specific game
+    mapping (uint => mapping (uint => uint)) timeoutTimestamp;
+
+    // A player could start a timeout without sharing the turn he played
+    // Therefore we save the last played turn (move and signature)
+    // to ensure the second player can play and stop the timeout
+    mapping (uint => mapping (uint => Turn)) timeoutTurn;
+
+
     // Simulate the turn from the board, user's move and input state
     function simulate(
         address boardHandlerAddress,
@@ -58,8 +79,8 @@ contract EthBoards {
         return ecrecover(message, v, r, s);
     }
 
-    // Test the victory of the board and call the terminate the game if victory
-    function claimVictory(
+    // Verify the two last turns and check if the current state of the game is legitimate
+    function checkTurnsLegitimacy(
         address boardHandlerAddress,
         uint boardId,
         uint gameId,
@@ -69,15 +90,13 @@ contract EthBoards {
         bytes32[2] memory s,
         uint8[2] memory v,
         uint8[121] memory inputState
-    ) public {
+    ) internal returns (uint8[121] memory currentState, uint latestTurn) {
         // Get the board
         BoardHandler boardHandler = BoardHandler(boardHandlerAddress);
         require(boardId < boardHandler.getBoardNumber(), "The board doesn't exist");
 
         // Check the game exist and is not finished
         require(!boardHandler.isGameOver(boardId, gameId), "The game is already over");
-
-        uint8[121] memory currentState;
 
         // If we begin from the first turn therefore the input state is the initial state from the board
         if (initialTurnNumber == 0) {
@@ -87,11 +106,11 @@ contract EthBoards {
         }
 
         // The nonce is a triplet containing: [boardId, gameId, turn]
-        uint turnNumber = initialTurnNumber;
+        latestTurn = initialTurnNumber;
         uint[3] memory nonce;
         nonce[0] = boardId;
         nonce[1] = gameId;
-        nonce[2] = turnNumber;
+        nonce[2] = latestTurn;
 
         // Check each turn has been signed by the correct player
         // And retrieve the final state
@@ -116,16 +135,147 @@ contract EthBoards {
             );
 
             // Increment the turn number for the second turn
-            nonce[2] += 1;  // TODO: Use SafeMath
-            turnNumber += 1;
+            nonce[2] += 1;
+            latestTurn += 1;
         }
-        turnNumber -= 1;
+
+        // We decrement latestTurn to get the number of latest played turn
+        return (currentState, latestTurn-1);
+    }
+
+    // Test the victory of the board and call the terminate the game if victory
+    function claimVictory(
+        address boardHandlerAddress,
+        uint boardId,
+        uint gameId,
+        uint initialTurnNumber,
+        uint8[4][2] memory move,
+        bytes32[2] memory r,
+        bytes32[2] memory s,
+        uint8[2] memory v,
+        uint8[121] memory inputState
+    ) public {
+        uint8[121] currentState;
+        uint latestTurn;
+
+        // Check the legitimacy of the latest turn
+        (currentState, latestTurn) = checkTurnsLegitimacy(
+            boardHandlerAddress,
+            boardId,
+            gameId,
+            initialTurnNumber,
+            move,
+            r,
+            s,
+            v,
+            inputState
+        );
 
         // Check if the final state is victoreious
         Board board = Board(boardHandler.getBoardContractAddress(boardId));
-        require(board.checkVictory(uint8(turnNumber%2), currentState), "The player hasn't won the game");
+        require(board.checkVictory(uint8(latestTurn%2), currentState), "The player hasn't won the game");
 
         // If the player won, terminate the game
-        boardHandler.finishGame(boardId, gameId, uint8(turnNumber%2));
+        boardHandler.finishGame(boardId, gameId, uint8(latestTurn%2));
+    }
+
+    // Start a timeout
+    function startTimeout(
+        address boardHandlerAddress,
+        uint boardId,
+        uint gameId,
+        uint initialTurnNumber,
+        uint8[4][2] memory move,
+        bytes32[2] memory r,
+        bytes32[2] memory s,
+        uint8[2] memory v,
+        uint8[121] memory inputState
+    ) public {
+        uint8[121] currentState;
+        uint latestTurn;
+
+        // Check the legitimacy of the latest turn
+        (currentState, latestTurn) = checkTurnsLegitimacy(
+            boardHandlerAddress,
+            boardId,
+            gameId,
+            initialTurnNumber,
+            move,
+            r,
+            s,
+            v,
+            inputState
+        );
+
+        // Verify there is no upper timeout turn
+        require(latestTurn > timeoutTurnNumber[boardId][gameId], "A upper timeout turn already exist");
+
+        // Set the timeout
+        timeoutTurnNumber[boardId][gameId] = latestTurn;
+        timeoutTimestamp[boardId][gameId] = block.timestamp;
+
+        // Store the last played turn to ensure it is shared to the players
+        timeoutTurn[boardId][gameId].move = move[1];
+        timeoutTurn[boardId][gameId].r = r[1];
+        timeoutTurn[boardId][gameId].s = s[1];
+        timeoutTurn[boardId][gameId].v = v[1];
+    }
+
+    // Stop the timeout
+    function stopTimeout(
+        address boardHandlerAddress,
+        uint boardId,
+        uint gameId,
+        uint initialTurnNumber,
+        uint8[4][2] memory move,
+        bytes32[2] memory r,
+        bytes32[2] memory s,
+        uint8[2] memory v,
+        uint8[121] memory inputState
+    ) public {
+        uint8[121] currentState;
+        uint latestTurn;
+
+        // Check the legitimacy of the latest turn
+        (currentState, latestTurn) = checkTurnsLegitimacy(
+            boardHandlerAddress,
+            boardId,
+            gameId,
+            initialTurnNumber,
+            move,
+            r,
+            s,
+            v,
+            inputState
+        );
+
+        // Verify that the turn is above the timeout turn
+        require(latestTurn > timeoutTurnNumber[boardId][gameId], "This turn is not above the timeout turn");
+
+        // Cancel timeout
+        timeoutTimestamp[boardId][gameId] = 0;
+    }
+
+    // Execute the timeout and kick the afk player
+    function executeTimeout(
+        address boardHandlerAddress,
+        uint boardId,
+        uint gameId,
+    ) {
+        // Get the board
+        BoardHandler boardHandler = BoardHandler(boardHandlerAddress);
+        require(boardId < boardHandler.getBoardNumber(), "The board doesn't exist");
+
+        // Check the game exist and is not finished
+        require(!boardHandler.isGameOver(boardId, gameId), "The game is already over");
+
+        // Check there is a pending timeout
+        require(timeoutTimestamp[boardId][gameId] > 0, "There is no pending timeout");
+
+        // Check the timeout time has been reached
+        require(block.timestamp > timeoutTimestamp[boardId][gameId] + timeoutTime, "The timeout is not reached yet");
+
+        // FInish the game with the latest player who played the turn as the winner
+        boardHandler.finishGame(boardId, gameId, uint8(timeoutTurnNumber[boardId][gameId]%2));
     }
 }
