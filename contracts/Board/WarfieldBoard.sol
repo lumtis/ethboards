033852d7e8f5bf649bc8d00e538b5d19f865eeb8
ltacker <lucas.bertrand.22@gmail.com>
2020-07-27 17:02:56 +0200

    Warfield board

--- contracts/Board/WarfieldBoard.sol
+++ contracts/Board/WarfieldBoard.sol
@@ -8,6 +8,28 @@ contract WarfieldBoard is Board {
 
     // This is no real chess, we check if the king has been killed to determine the victory
     function checkVictory(uint8 player, uint8[121] calldata state) external override view returns(bool) {
-        return false;
+        if (player == 0) {
+            // Player A win if all red bases are captured
+            for (uint8 i = 0; i < 40; i++) {
+                if (state.isAlive(i)) {
+                    // Pawn 4 should be a red base
+                    if (state.getPawnType(i) == 4) {
+                        return false;
+                    }
+                }
+            }
+        } else {
+            // Player B win if all blue bases are captured
+             for (uint8 i = 0; i < 40; i++) {
+                if (state.isAlive(i)) {
+                    // Pawn 0 should be a blue base
+                    if (state.getPawnType(i) == 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
     }
 }
